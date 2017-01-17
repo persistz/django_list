de69aea46ac78d04c2ef535583073b6ec112d169
George.Zhao <z.z206@163.com>
2017-01-17 21:27:34 +0800

    completion functional test case

--- function_tests/tests.py
+++ function_tests/tests.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -19,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 她注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn("To-Do", self.browser.title)
@@ -36,15 +38,16 @@ class NewVisitorTest(unittest.TestCase):
         # 伊迪丝的爱好是使用假蝇做饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
 
-        # 她按回车键后，页面更新了
+        # 她按回车键后，被带到了一个新的URL
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-        table =self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_rows_in_list_table('1: Buy peacock feathers')
+
         # self.assertTrue(
         #     any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table"
         # )
-        self.check_for_rows_in_list_table('1: Buy peacock feathers')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
@@ -54,18 +57,37 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
         self.check_for_rows_in_list_table('1: Buy peacock feathers')
         self.check_for_rows_in_list_table('2: Use peacock feathers to make a fly')
 
-        # 伊迪丝想知道这个网站是否会记住她的清单
-        # 她看到网站为她生成了一个唯一的URL
-        # 而且页面中有一些文字解说这个功能
-        # 她访问那个URL，发现她的待办事项列表还在
-        # 她很满意，去睡觉了
-        self.fail("Finish the test!")
+        ## 现在一个叫做弗朗西斯的新用户访问了网站
+        ## 确保伊迪斯的信息不会从cookie中泄露出来
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
 
-if __name__ == '__main__':
-    unittest.main()
+        # 弗朗西斯访问首页
+        # 页面中看不到伊迪斯的清淡
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
+        # 弗朗西斯输入一个新待办事项，新建一个清单
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 弗朗西斯获得了唯一的URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 伊迪丝依然没有在这个页面出现
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 两个人都很满意，去睡觉了
+
+# if __name__ == '__main__':
+#     unittest.main()
